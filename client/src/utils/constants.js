819f6de18266b06e71682e34512d78f81a4ff859
Yash-Dabhade <yashdabhade215@gmail.com>
2024-04-13 12:29:46 +0530

    Updated Backend Deployment Link

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,6 +2,6 @@
 // const baseURL = "http://localhost:4000/api/v1";
 
 //Deployed
-const baseURL = "https://genzgames-production.up.railway.app";
+const baseURL = "https://genzgames-production.up.railway.app/api/v1";
 
 export { baseURL };
